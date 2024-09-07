8767e282b2893b75d50b6f007f59c74947339331
Sumedh314 <sumedh3141@gmail.com>
2024-09-07 15:46:18 -0500

    fixed some bugs

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from duet_3d_dev import Duet3dDev
-from ScopeFoundry import BaseMicroscopeApp
+from ScopeFoundry import BaseApp
 
 
-class Duet3d(BaseMicroscopeApp):
+class Duet3d(BaseApp):
 
     name = 'microscope'
 
@@ -10,6 +10,8 @@ class Duet3d(BaseMicroscopeApp):
         from duet_3d_hw import Duet3dHW
         self.add_hardware(Duet3dHW(self))
 
+        self.ui.show()
+        self.ui.activateWindow()
 
 
 duet = Duet3dDev()
